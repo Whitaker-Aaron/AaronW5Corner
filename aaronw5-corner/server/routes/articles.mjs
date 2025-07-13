c721da2e22dc277046006cc3af1e863d56747654
Aaron Whitaker <aaronwhitaker690@gmail.com>
2025-07-12 18:11:46 -0700

    Reconfigured Backend

--- aaronw5-corner/server/routes/articles.mjs
+++ aaronw5-corner/server/routes/articles.mjs
@@ -1,8 +1,27 @@
 import express from "express";
-import db from "../db/conn.mjs";
+//import db from "../db/conn.mjs";
 import { ObjectId } from "mongodb";
 
+
+
 const router = express.Router();
+
+
+
+//CONNECT TO DB
+import { MongoClient } from "mongodb";
+const connectionString = process.env.ATLAS_URI || ATLAS_URI;
+const client = new MongoClient(connectionString);
+
+let conn;
+try {
+    conn = await client.connect();
+} catch (e) {
+    console.error(e);
+}
+let db = conn.db("AaronW5Corner");
+
+
 
 // Get a list of 50 posts
 router.get("/", async (req, res) => {
