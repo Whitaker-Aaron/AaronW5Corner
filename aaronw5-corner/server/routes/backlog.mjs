1c73e46c676de9b506a387e73d44d20ffe329260
Aaron Whitaker <89281972+Whitaker-Aaron@users.noreply.github.com>
2026-06-13 18:37:21 -0700

    fix date

--- aaronw5-corner/server/routes/backlog.mjs
+++ aaronw5-corner/server/routes/backlog.mjs
@@ -57,10 +57,10 @@ router.get("/backlogged/get", async (req, res) => {
     res.send(results).status(200);
 });
 
-router.get("/backlogged/get/:year", async (req, res) => {
+router.get("/completed/get/:id", async (req, res) => {
     let collection = await db.collection("Backlog");
     //const { limit } = req.body
-    let query = {status: 'backlog', date: {$gte: new Date(`${req.params.year}-01-01`), $lte: new Date(`${req.params.year}-12-31`)}};
+    let query = {status: 'backlog', date: {$gte: new Date(`${req.params.id}-01-01`), $lte: new Date(`${req.params.id}-12-31`)}};
     let results = await collection.find(query).sort({date: -1})
         .toArray();
     console.log(results);
