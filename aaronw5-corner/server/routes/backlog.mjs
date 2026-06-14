72a2515ad8c6564f268a851f3ccb689a46eca670
Aaron Whitaker <89281972+Whitaker-Aaron@users.noreply.github.com>
2026-06-13 18:34:13 -0700

    add new endpoint

--- aaronw5-corner/server/routes/backlog.mjs
+++ aaronw5-corner/server/routes/backlog.mjs
@@ -57,6 +57,17 @@ router.get("/backlogged/get", async (req, res) => {
     res.send(results).status(200);
 });
 
+router.get("/backlogged/get/:year", async (req, res) => {
+    let collection = await db.collection("Backlog");
+    //const { limit } = req.body
+    let query = {status: 'backlog', orderDate: {$gte: new Date(`${req.params.year}-01-01`), $lte: new Date(`${req.params.year}-12-31`)}};
+    let results = await collection.find(query).sort({date: -1})
+        .toArray();
+    console.log(results);
+
+    res.send(results).status(200);
+});
+
 router.get("/completed/get", async (req, res) => {
     let collection = await db.collection("Backlog");
     //const { limit } = req.body
